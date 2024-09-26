69343cf3a87cd072dfa123bfeae3f4cfc1bdef71
Nuriddinov Subkhiddin <subkhiddinn1@gmail.com>
2024-09-27 00:31:49 +0500

    created Xcode project

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Nuriddinov Subkhiddin on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
